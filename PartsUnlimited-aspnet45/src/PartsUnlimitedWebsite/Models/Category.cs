59221197dfc86c336a8ec7a3d40867d1150a323c
rranj1919 <rahulranjancts18@gmail.com>
2020-05-26 23:32:42 +0530

    Category change. Fixes #1Category change. Fixes #1

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class Category
     {
-        // My ford change
+        // My fordd change
         public int CategoryId { get; set; }
 
         [Required]
